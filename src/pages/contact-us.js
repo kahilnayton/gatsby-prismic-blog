5f0000d26096d7a503eb3852ce17d6ad0916c580
kahil nayton <kahilnayton@gmail.com>
2020-05-22 09:59:19 -0400

    forms

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -48,6 +48,30 @@ const Form = styled.form`
     resize: none;
   }
 `;
+const Form2 = styled.form`
+  padding: 10px;
+  background: orangered;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+
+  input {
+    margin-bottom: 1rem;
+    border-radius: 4px;
+    height: 4rem;
+    border: 1px solid #eee;
+    width: 100%;
+  }
+
+  textarea {
+    margin-bottom: 1rem;
+    border-radius: 4px;
+    height: 10rem;
+    border: 1px solid #eee;
+    width: 100%;
+    resize: none;
+  }
+`;
 
 const Button = styled.button`
   background: orange;
@@ -77,10 +101,12 @@ const ContactUs = props => {
           }
         />
         <Form
-          name='contact-us'
+          name='contact-us-two'
           method='Post'
           data-netlify='true'
+          data-netlify-honeypot="bot-field"
           action='/contact-success'>
+          
           <input type='hidden' name='form-name' value='contact-us'/>
           {props.data.prismic.allContact_pages.edges[0].node.form_fields.map(
             (field, i) => {
@@ -110,6 +136,43 @@ const ContactUs = props => {
           )}
           <Button type="submit">Submit</Button>
         </Form>
+
+        <Form2
+          name='contact-us'
+          method='Post'
+          data-netlify='true'
+          data-netlify-honeypot="bot-field"
+          action='/contact-success'>
+          
+          <input type='hidden' name='form-name' value='contact-us'/>
+          {props.data.prismic.allContact_pages.edges[0].node.form_fields.map(
+            (field, i) => {
+              if (field.field_type === 'textarea') {
+                return (
+                  <div key={i}>
+                    <textarea
+                      name={field.name}
+                      required={field.required === 'Yes'}
+                      placeholder={field.name}
+                    />
+                  </div>
+                );
+              } else {
+                return (
+                  <div key={i}>
+                    <input
+                      name={field.name}
+                      placeholder={field.name}
+                      required={field.required === 'Yes'}
+                      type={field.field_type}
+                    />
+                  </div>
+                );
+              }
+            }
+          )}
+          <Button type="submit">Submit</Button>
+        </Form2>
       </ContentWrapper>
     </Layout>
   );
